1141171ee2d2cf1ea8eac4336a3b3bae2d2d0713
Igashi Michael <131265224+Mikkybeardless@users.noreply.github.com>
2026-01-19 09:04:22 +0100

    Feat/categories (#12)
    * feat: set up sqllite with prisma and created categories service
    
    * feat: add input validation for category request body
    
    * fix: removed generated prisma client from git
    
    * chore(ci): ensure prisma client is generated on install
    
    * feat: created categories dynamic id route
    
    * feat: add type of param contex to routes

---
+++ app/api/(modules)/categories/[id]/route.ts
@@ -0,0 +1,81 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { CategoriesService } from '../categories.service'
+
+type RouterContex = {
+  params: Promise<{
+    id: string
+  }>
+}
+
+const categoriesService = new CategoriesService()
+export async function DELETE(_: NextRequest, { params }: RouterContex) {
+  try {
+    const id = Number((await params).id)
+    await categoriesService.delete(id)
+    return NextResponse.json(
+      {
+        message: 'Category successfully deleted',
+        success: true,
+      },
+      { status: 200 }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An error occured while deleting category',
+        error: (error as Error).message,
+        success: false,
+      },
+      { status: 500 }
+    )
+  }
+}
+
+export async function GET(_: NextRequest, { params }: RouterContex) {
+  try {
+    const id = Number((await params).id)
+    const category = await categoriesService.findById(id)
+    return NextResponse.json(
+      {
+        message: 'Category successfully retrieved',
+        success: true,
+        data: category,
+      },
+      { status: 200 }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An error occured while fetching category',
+        error: (error as Error).message,
+        success: false,
+      },
+      { status: 500 }
+    )
+  }
+}
+
+export async function PUT(req: NextRequest, { params }: RouterContex) {
+  try {
+    const id = Number((await params).id)
+    const body = await req.json()
+    const category = await categoriesService.update(id, body)
+    return NextResponse.json(
+      {
+        message: 'Category successfully updated',
+        success: true,
+        data: category,
+      },
+      { status: 200 }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'An error occured while updating category',
+        error: (error as Error).message,
+        success: false,
+      },
+      { status: 500 }
+    )
+  }
+}
